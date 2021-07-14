1964ebb9050248402ffca75f781d2fc92789dec7
rainmaker6 <masteroftheirclassic@gmail.com>
2021-07-14 09:19:31 +0530

    one

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main(){
-	println!("Hello, world!");
-}
+fn main() {
+	let a = Employee{
+	   company:String::from("Google"),
+	   name:String::from("John"),
+	   age:50
+	};
+	let b = Employee {
+	   company:String::from("Google"),
+	   name:String::from("Kannan"),
+	   age:32
+	};
+	let elder = who_is_elder(a,b);
+	println!("elder is:");
+
+	display(elder);
+ }
+ fn who_is_elder (a:Employee,b:Employee)->Employee {
+	if a.age>b.age {
+	   return a;
+	} else {
+	   return b;
+	}
+ }
+ fn display(emp:Employee) {
+	println!("Name is :{} company is {} age is {}",emp.name,emp.company,emp.age);
+ }
+ struct Employee {
+	name:String,
+	company:String,
+	age:u32
+ }
